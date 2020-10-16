629de3d8ac66171b8c7879ce560ac300df24b99c
EkaKakalashvili <72973036+EkaKakalashvili@users.noreply.github.com>
2020-10-16 14:50:31 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Size of the massive is: ");
+        int size= s.nextInt();
+        System.out.println("Enter the elements of massive: ");
+        int[] array = new int[length];
+        for(int i=0;i<size;i++){
+            array[i] = s.nextInt();
+        }
+        System.out.println("Minimum positive value in this massive is: " + notContains(array));
+    }
+    public static int notContains(int[] array){
+        //Arrays.sort(array);
+        int minValue = 1; // minimum positive value
+        for(int i =0; i<array.length;i++){
+            if (array[i] == minValue){
+                ++minValue;
+            }
+        }
+        return minValue;
+    }
+}
